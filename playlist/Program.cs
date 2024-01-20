fe8aa39e2de3480f082f26a8a80947f4327cc35b
bachar-alhamada <bachar.alhamada76@gmail.com>
2024-01-20 03:14:03 -0600

    Add project files.

---
+++ playlist/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace playlist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stacks_and_Queues addSong = new Stacks_and_Queues();
+            bool exitProgram = false;
+            while (exitProgram == false)
+            {
+                Console.WriteLine(" ------------------------------------------ ");
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Add a song to your playlist");
+                Console.WriteLine("2. Play the next song in your playlist");
+                Console.WriteLine("3. Skip the next song");
+                Console.WriteLine("4. Rewind one song");
+                Console.WriteLine("5. Exit");
+
+                Console.Write("Enter your choice (1-5): ");
+                string userinput = Console.ReadLine();
+               
+
+
+                switch (userinput)
+                {
+                    case "1":
+                        addSong.AddSong();
+                        break;
+
+                    case "2":
+                        addSong.PlayTheNextSong();
+                        break;
+
+                    case "3":
+                        addSong.SkipTheNextSong();
+                        break;
+
+                    case "4":
+                        addSong.rewindOneSong();
+                        break;
+
+                    case "5":
+                        // Exit the program
+
+                        Console.WriteLine("you exit the program  ");
+                        exitProgram = true;
+                        Thread.Sleep(5000); // Sleeps for 5000 milliseconds, or 5 seconds
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                        break;
+                }
+
+                Console.WriteLine(); // Add an empty line for better readability
+            }
+        }
+
+    }
+    
+}
---
+++ playlist/Stacks and Queues.cs
@@ -0,0 +1,71 @@
+﻿  using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playlist
+{
+    internal  class Stacks_and_Queues
+    {
+        Queue<string> playlist = new Queue<string>();
+        Stack<string> stackplayList = new Stack<string>();
+
+
+        public  string AddSong()
+        {
+
+            Console.Write(" enter the song that you want to add :> ");
+            string songName = Console.ReadLine();
+            // Add the user-input song to the playlist
+            playlist.Enqueue(songName);
+            Console.Write($"{songName} has been added to your playlist ");
+            Console.WriteLine($"nnnn {playlist.Count} ");
+            if (playlist.Count > 0)
+            {
+                Console.WriteLine($"Next song: {playlist.Peek()}");
+            }
+
+            return songName;
+        }
+        public void PlayTheNextSong()
+        {
+            string currentSong = playlist.Peek();
+            string nextSong = playlist.Dequeue();
+            Console.Write($" now :{currentSong}  ");
+            Console.Write($" next :{nextSong} ");
+
+
+        }
+        public void SkipTheNextSong()
+        {
+            string SkipNext = playlist.Dequeue();
+            string NextofNext = playlist.Dequeue();
+            Console.Write($" you skip  {SkipNext} to {NextofNext}  ");
+
+        }
+        public void rewindOneSong()
+        {
+            foreach (string item in playlist)
+            {
+                stackplayList.Push(item);
+
+            }
+            string topSong = stackplayList.Pop(); // Temporarily remove the top song
+            string previousSong = stackplayList.Peek();
+            Console.WriteLine($" you rewind one song to {previousSong} ");
+            stackplayList.Push(topSong); // Put the top song back
+
+
+        }
+
+
+
+
+
+
+    }
+}
